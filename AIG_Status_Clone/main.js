495c005ff120b0c1585ea6adfc61857fbde7a66f
moseleygj <moseleygj@gmail.com>
2017-11-11 23:43:05 -0500

    update

--- AIG_Status_Clone/main.js
+++ AIG_Status_Clone/main.js
@@ -1,10 +1,9 @@
 function myMap() {
-var mapProp= {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-};
+
+  var mapProp= {
+      center:new google.maps.LatLng(51.508742,-0.120850),
+      zoom:5,
+  };
+
 var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
-
-<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY&callback=myMap"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCveDGXm_ZCHx5eZWLMu5Ld_0zMRqgOUjU&callback=myMap"></script>
