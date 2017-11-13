bc26353490daa7eb410012c88a9fba16123064ab
moseleygj <moseleygj@gmail.com>
2017-11-12 21:59:28 -0500

    update

--- AIG_Status_Clone/main.js
+++ AIG_Status_Clone/main.js
@@ -1,9 +1,25 @@
 function myMap() {
 
   var mapProp= {
-      center:new google.maps.LatLng(51.508742,-0.120850),
-      zoom:5,
+      center:new google.maps.LatLng(40.8728,-73.8287),
+      zoom:14,
   };
 
 var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+//Get current location
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude;
 }
