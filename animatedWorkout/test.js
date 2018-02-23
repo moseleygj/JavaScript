fcb8415f711938374f7444b0fa5e61cb588e9bb7
moseleygj <moseleygj@gmail.com>
2018-02-23 15:53:55 -0500

    AJAX Logic update

--- animatedWorkout/test.js
+++ animatedWorkout/test.js
@@ -1,17 +1,4 @@
-// window.onload=function(){
-//   var xhttp = new XMLHttpRequest();
-//
-// xhttp.onreadystatechange = function() {
-//
-// if(xhttp.readyState == 4 && xhttp.status == 200){
-//
-// var myObj = JSON.parse(xhttp.responseText);
-// console.log(myObj.pullups.image);
-//   }
-// };
-// xhttp.open("GET", "main.json", true);
-// xhttp.send(); //go bring me the data
-// };
+
 var xhttp = new XMLHttpRequest();
 var myObj;
 
@@ -24,3 +11,30 @@ xhttp.onreadystatechange = function()
 };
 xhttp.open("GET", "main.json", true);
 xhttp.send(); //go bring me the data
+
+
+function showFit(exercise)
+{
+document.getElementById("exerciseImg").src="gifs/"+exercise;
+	}
+	function randomPlaceHolder()
+	{
+		var x= Math.floor(Math.random()* 11)+1;
+	document.getElementById("exerciseImg").src="loading/loading"+x+".gif";
+		}
+
+//check which option is selected
+
+function hideShowPreview(){
+	if ( document.getElementById("showPreview").checked == true){
+		document.getElementById("exerciseImg").style.display="block";
+	}
+	else if (document.getElementById("showPreview").checked == false){
+		document.getElementById("exerciseImg").style.display="none";
+	}
+	else{
+		alert("Error!");
+	}
+			//decrearse workoutCell size ;slide-left(slow)
+			//increase size of workout list;slide (left);
+		}
