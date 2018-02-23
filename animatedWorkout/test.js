05aa01ebe4b8cdadc65df2dbc99be2dc259697aa
moseleygj <moseleygj@gmail.com>
2018-02-23 16:19:58 -0500

    AJAX Logic update

--- animatedWorkout/test.js
+++ animatedWorkout/test.js
@@ -16,25 +16,30 @@ xhttp.send(); //go bring me the data
 function showFit(exercise)
 {
 document.getElementById("exerciseImg").src="gifs/"+exercise;
-	}
-	function randomPlaceHolder()
+}
+
+
+function randomPlaceHolder()
+{
+var x= Math.floor(Math.random()* 11)+1;
+document.getElementById("exerciseImg").src="loading/loading"+x+".gif";
+}
+
+
+function hideShowPreview()
+{
+	if ( document.getElementById("showPreview").checked == true)
 	{
-		var x= Math.floor(Math.random()* 11)+1;
-	document.getElementById("exerciseImg").src="loading/loading"+x+".gif";
-		}
-
-//check which option is selected
-
-function hideShowPreview(){
-	if ( document.getElementById("showPreview").checked == true){
 		document.getElementById("exerciseImg").style.display="block";
+		//increase size of workout list;slide (left);
+
 	}
-	else if (document.getElementById("showPreview").checked == false){
+	else if (document.getElementById("showPreview").checked == false)
+	{
 		document.getElementById("exerciseImg").style.display="none";
+		//decrearse workoutCell size ;slide-left(slow)
 	}
 	else{
 		alert("Error!");
 	}
-			//decrearse workoutCell size ;slide-left(slow)
-			//increase size of workout list;slide (left);
-		}
+}
