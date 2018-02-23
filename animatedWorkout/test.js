d86afcfd2770d8dbf9bd66616abe2ec57122742f
moseleygj <moseleygj@gmail.com>
2018-02-23 13:41:30 -0500

    AJAX Logic update

--- animatedWorkout/test.js
+++ animatedWorkout/test.js
@@ -1,14 +1,26 @@
-window.onload=function(){
-  var xhttp = new XMLHttpRequest();
+// window.onload=function(){
+//   var xhttp = new XMLHttpRequest();
+//
+// xhttp.onreadystatechange = function() {
+//
+// if(xhttp.readyState == 4 && xhttp.status == 200){
+//
+// var myObj = JSON.parse(xhttp.responseText);
+// console.log(myObj.pullups.image);
+//   }
+// };
+// xhttp.open("GET", "main.json", true);
+// xhttp.send(); //go bring me the data
+// };
+var xhttp = new XMLHttpRequest();
+var myObj;
 
-xhttp.onreadystatechange = function() {
-
-if(xhttp.readyState == 4 && xhttp.status == 200){
-
-var myObj = JSON.parse(xhttp.responseText);
-console.log(myObj.pullups.image);
-  }
+xhttp.onreadystatechange = function()
+	{
+		if(xhttp.readyState == 4 && xhttp.status == 200)
+		{
+				myObj = JSON.parse(xhttp.responseText);
+		}
 };
 xhttp.open("GET", "main.json", true);
 xhttp.send(); //go bring me the data
-};
