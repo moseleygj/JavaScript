370ebc1c6c55f28a1a84b753173afed418597827
moseleygj <moseleygj@gmail.com>
2018-10-23 23:25:04 -0400

    react testing

---
+++ reacttest/src/index_files/main_002.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("main",{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+
+
+/***/ })
+
+})
+//# sourceMappingURL=main.c280b5ef3096508db536.hot-update.js.map
