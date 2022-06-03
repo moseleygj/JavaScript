2a42add51d97f78b713cc0b64fd5bd315e805218
moseleygj <moseleygj@gmail.com>
2022-06-02 21:25:03 -0400

    correct note section  & alignment of center div

--- animatedWorkout/testRedesign.js
+++ animatedWorkout/testRedesign.js
@@ -26,3 +26,14 @@ function showFit(a)
   document.getElementById("subContdivainter").style.display="block";
 document.getElementById("sampExercise").src=a;
 }
+
+function showNote(){
+		document.getElementById("tooltip").style.visibility="visible";
+		document.getElementById("tooltipBody").style.visibility="visible";
+
+}
+
+
+function hideNote(){
+		document.getElementById("tooltip").style.visibility="hidden";
+}
